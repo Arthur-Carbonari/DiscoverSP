d7cd37acda216ca9ef6bff64d4abb77f309d0203
Arthur Martins <arthur.carbonarimartins@student.griffith.ie>
2021-05-31 23:56:49 +0100

    finished

--- script.js
+++ script.js
@@ -27,3 +27,63 @@ allDropdownMenus.forEach(function(item) {
     menu.classList.toggle("showDropDownMenu");
   });
 });
+
+let form = document.querySelector("#commentForm");
+let buttonSend = document.querySelector("#buttonSend");
+
+if (form != null){
+  console.log("erro aqui");
+
+  buttonSend.addEventListener("click", function(e){
+
+    // name = document.querySelector("#formStart").nameInput.value;
+
+
+    let destinatary = form.destinatary.value;
+    let name = form.nameInput.value;
+    console.log(name);
+    let message = form.querySelector("#messageField").value;
+    console.log(message);
+
+    if (destinatary == "" || name == "" || message == ""){
+      form.querySelector(".warning").classList.remove("hideDisplay");
+      return;
+    }
+
+    form.querySelector(".warning").classList.add("hideDisplay");
+
+    let confirmationFields = form.querySelectorAll("#comfirmationField main p");
+
+    confirmationFields[0].textContent = "Your name: " + name;
+    confirmationFields[1].textContent = "Destinatary: " + destinatary;
+    confirmationFields[3].textContent = message;
+
+    form.querySelector(".blackBackground").classList.remove("hideDisplay");
+
+
+  });
+
+
+  let buttonCancel = document.querySelector("#buttonCancel");
+  buttonCancel.addEventListener("click", function(e){
+
+    form.querySelector(".blackBackground").classList.add("hideDisplay");
+
+  });
+
+
+  let buttonConfirm = document.querySelector("#buttonConfirm");
+  buttonConfirm.addEventListener("click", function(e){
+
+    form.querySelector(".blackBackground").classList.add("hideDisplay");
+
+    form.querySelector(".comfirmationMessage").classList.remove("hideDisplay");
+  });
+}
+
+let hamburguer = document.querySelector("#hamburguer");
+hamburguer.addEventListener("click", function(e){
+
+  document.querySelector("#masthead nav").classList.toggle("showNavMenu");
+  
+});

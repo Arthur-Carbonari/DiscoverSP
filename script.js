1d70ab5bbce87ae4f8258f8ba0695d65a17aef1b
Arthur Martins <arthur.carbonarimartins@student.griffith.ie>
2021-05-31 01:54:25 +0100

    templates ready

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-let indexPlusButtons = document.querySelectorAll(".indexArticle .buttonPlus");
+let indexPlusButtons = document.querySelectorAll("article .buttonPlus");
 indexPlusButtons.forEach(function(item) {
   item.addEventListener("click", function(e){
-    e.target.classList.toggle("buttonExit");
+
+    let thisElement = e.target;
+    thisElement.classList.toggle("buttonExit");
+
+    let sideMenu = thisElement.parentElement.querySelector(".sideMenu");
+    sideMenu.classList.toggle("showSide");
+
+  });
+});
+
+let allDropdownMenus = document.querySelectorAll(".dropdownMenu");
+allDropdownMenus.forEach(function(item) {
+
+  item.addEventListener("click", function(e){
+
+    let menu = e.target.parentElement;
+
+    if (!menu.classList.contains("showDropDownMenu")){
+      allDropdownMenus.forEach(function(item2) {
+        item2.parentElement.classList.remove("showDropDownMenu");
+      });
+    }
+
+    menu.classList.toggle("showDropDownMenu");
   });
 });
